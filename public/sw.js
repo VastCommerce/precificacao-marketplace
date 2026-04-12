4cb31211e5c2352b41c5daa2ffcbc864806626de
Regis Ramos <geregisramos@gmail.com>
2026-04-11 23:44:27 -0300

    adiciona sw.js

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'precificacao-v2'
+
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request))
+})
